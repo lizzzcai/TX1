cf5e621864a69ee8fc690b6da0d288cb4151818e
Lawrence Cai <lilcolinn@gmail.com>
2017-07-15 04:13:18 +0800

    Update basic.sh
    update

--- basic.sh
+++ basic.sh
@@ -8,35 +8,7 @@ sudo apt-get update -y
 sudo apt-get purge libreoffice* -y
 #sudo apt autoremove -y
 
-#OpenCV build dependencies
-# Some general development libraries
-sudo apt-get -y install build-essential make cmake cmake-curses-gui g++
-# libav video input/output development libraries
-sudo apt-get -y install libavformat-dev libavutil-dev libswscale-dev
-# Video4Linux camera development libraries
-sudo apt-get -y install libv4l-dev
-# Eigen3 math development libraries
-sudo apt-get -y install libeigen3-dev
-# OpenGL development libraries (to allow creating graphical windows)
-sudo apt-get -y install libglew-dev
-# GTK development libraries (to allow creating graphical windows)
-sudo apt-get -y install libgtk2.0-dev
-
-sudo apt-get -y install libqt4-dev python-dev python-numpy python-py python-pytest
-
-sudo apt-get install \
-    libglew-dev \
-    libtiff5-dev \
-    zlib1g-dev \
-    libjpeg-dev \
-    libpng12-dev \
-    libjasper-dev \
-    libavcodec-dev \
-    libavformat-dev \
-    libavutil-dev \
-    libpostproc-dev \
-    libswscale-dev \
-    libeigen3-dev \
-    libtbb-dev \
-    libgtk2.0-dev \
-    pkg-config
+# install browser
+sudo apt-get install firefox 
+# sudo apt-get install dillo
+sudo reboot
